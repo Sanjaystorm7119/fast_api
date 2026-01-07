9abc93d5066257e4f5dee83b8f8a254d819a16e8
Sanjay b <sanjaysrivastavab@gmail.com>
2026-01-08 01:02:33 +0530

    todo proj

--- todo/database.py
+++ todo/database.py
@@ -4,7 +4,7 @@ from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm import declarative_base
 
 
-SQLALCEMY_DATABASE_URL = 'sqlite.///./todos.db'
+SQLALCEMY_DATABASE_URL = 'sqlite:///./todos.db'
 
 
 engine = create_engine(SQLALCEMY_DATABASE_URL, connect_args={'check_same_thread':False})
@@ -12,10 +12,6 @@ engine = create_engine(SQLALCEMY_DATABASE_URL, connect_args={'check_same_thread'
 session_local = sessionmaker(autocommit= False , autoflush=False, bind=engine)
 
 Base = declarative_base() #object of db
-
-
- 
-
 
 
 """
