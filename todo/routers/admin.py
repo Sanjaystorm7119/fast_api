ba2a7e63ac761afd87978715c45f3a1d90d97e73
Sanjay b <sanjaysrivastavab@gmail.com>
2026-01-13 19:30:34 +0530

    add get , delete req to admin

--- todo/routers/admin.py
+++ todo/routers/admin.py
@@ -27,3 +27,16 @@ async def read_all(user : user_dependency , db : db_dependency):
     if user is None or user.get('user_role') != "admin" :
         raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="authentication failed")
     return db.query(Todos).all()
+
+@router.delete('/todo/{todo_id}',status_code=status.HTTP_204_NO_CONTENT)
+async def delete_todo_by_id(user : user_dependency , db: db_dependency , todo_id : int = Path(gt=0)):
+    if user is None or user.get('user_role') != "admin" :
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="authentication failed")
+    todo_model = db.query(Todos).filter(Todos.id == todo_id).first()
+    if todo_model is None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="not found")
+    db.query(Todos).filter(Todos.id == todo_id).delete()
+    db.commit()
+    
+    
+    
