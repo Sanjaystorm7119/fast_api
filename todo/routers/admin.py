a8654273e83c0632f6456b5bc1bcca5195bd4b4c
Sanjay b <sanjaysrivastavab@gmail.com>
2026-01-13 20:50:55 +0530

    feat : password reset : user and admin

--- todo/routers/admin.py
+++ todo/routers/admin.py
@@ -1,10 +1,10 @@
-from fastapi import  Depends , HTTPException, status , Path , APIRouter
-from models import Todos
+from fastapi import  Depends , HTTPException, status , Path , APIRouter 
+from models import Todos, Users
 from database import engine, session_local
-from typing import Annotated
+from typing import Annotated , Optional
 from sqlalchemy.orm import Session 
-from pydantic import BaseModel, Field
-from .auth import get_current_user
+from pydantic import BaseModel, Field , EmailStr
+from .auth import get_current_user, bcrypt_context
 # from routers import auth
 
 router = APIRouter(
@@ -18,9 +18,13 @@ def get_db():
         yield db 
     finally :
         db.close()
+
         
 db_dependency = Annotated[Session, Depends(get_db)]
 user_dependency = Annotated[dict, Depends(get_current_user)]
+
+class ResetPasswordRequest(BaseModel):
+    new_password: str = Field(min_length=4)
 
 @router.get('/todo' , status_code=status.HTTP_200_OK)
 async def read_all(user : user_dependency , db : db_dependency):
@@ -37,6 +41,14 @@ async def delete_todo_by_id(user : user_dependency , db: db_dependency , todo_id
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="not found")
     db.query(Todos).filter(Todos.id == todo_id).delete()
     db.commit()
-    
-    
+
+@router.put('/reset-password/{user_id}', status_code=status.HTTP_204_NO_CONTENT)
+async def reset_password(user: user_dependency, db: db_dependency, reset_request: ResetPasswordRequest, user_id: int = Path(gt=0)):
+    if user is None or user.get('user_role') != "admin":
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="authentication failed")
+    user_model = db.query(Users).filter(Users.id == user_id).first()
+    if user_model is None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="user not found")
+    user_model.hashed_pass = bcrypt_context.hash(reset_request.new_password)
+    db.commit()
     
---
+++ todo/routers/users.py
@@ -0,0 +1,52 @@
+from fastapi import  Depends , HTTPException, status , Path , APIRouter 
+from models import Todos , Users
+from database import engine, session_local
+from typing import Annotated , Optional
+from sqlalchemy.orm import Session 
+from pydantic import BaseModel, Field , EmailStr
+from .auth import get_current_user , bcrypt_context
+from passlib.context import CryptContext
+
+# from routers import auth
+
+router = APIRouter(
+    prefix='/user',
+    tags=['user']
+
+)
+def get_db():
+    db = session_local()
+    try :
+        yield db 
+    finally :
+        db.close()
+
+class user_verification(BaseModel):
+    password : str 
+    new_password : str  = Field(min_length=6)
+
+
+db_dependency = Annotated[Session, Depends(get_db)]
+user_dependency = Annotated[dict, Depends(get_current_user)]
+bcrypt_context = CryptContext(schemes=['bcrypt'], deprecated='auto')
+
+
+@router.get('/',status_code=status.HTTP_200_OK)
+async def get_user(user : user_dependency , db : db_dependency ):
+    if user is None:
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="authentication failed")
+    return db.query(Users).filter(Users.id == user.get('userid')).first()
+
+
+@router.put('/password/',status_code=status.HTTP_204_NO_CONTENT)
+async def update_user_details(user : user_dependency , db : db_dependency , user_verification : user_verification ):
+    if user is None :
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="authentication failed")
+    
+    user_model = db.query(Users).filter(Users.id == user.get('userid')).first()
+    if not bcrypt_context.verify(user_verification.password , user_model.hashed_pass):
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED , detail="unauthorised")
+    
+    user_model.hashed_pass = bcrypt_context.hash(user_verification.new_password)
+    db.commit()
+    
